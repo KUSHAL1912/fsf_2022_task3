218fe0158387b7dd5ad13dbe9a6ed9fc2839374c
KUSHAL <81311270+KUSHAL1912@users.noreply.github.com>
2022-04-05 01:41:42 +0530

    Add files via upload

---
+++ fossee.js
@@ -0,0 +1,10 @@
+function prom(){
+    alert("For more information please view the bottom👇 section of the page");
+}
+function promp(){
+  var jo=  alert("You will be  directed to a google form ");
+    
+    if(!jo){
+    window.location.href="https://docs.google.com/forms/d/e/1FAIpQLSfcGJSeLmKuOEEidUOJhKnDZSdcsPkSwWw3rQUtfR8LIRZugA/viewform?usp=sf_link"
+    }
+}
