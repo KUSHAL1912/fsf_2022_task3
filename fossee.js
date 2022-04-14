4ef9074610608e0c3e147ff5ec1e761c63477fd7
KUSHAL <81311270+KUSHAL1912@users.noreply.github.com>
2022-04-14 21:58:03 +0530

    Update fossee.js

--- fossee.js
+++ fossee.js
@@ -2,9 +2,17 @@ function prom(){
     alert("For more information please view the bottom👇 section of the page");
 }
 function promp(){
-  var jo=  alert("You will be directed to a google form ");
+  var jo=  alert("You will be  directed to a google form ");
     
     if(!jo){
     window.location.href="https://docs.google.com/forms/d/e/1FAIpQLSfcGJSeLmKuOEEidUOJhKnDZSdcsPkSwWw3rQUtfR8LIRZugA/viewform?usp=sf_link"
     }
+}
+function map(){
+  if(mapfossee){
+    x="https://goo.gl/maps/Yx7Q6y1MQfDvVMHu7";
+    
+    window.location.href=x;
+    
+  }
 }
